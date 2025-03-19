bc81ac77a90a9f7710cfe44927a824effa382894
Nivarcs <ai.tech.nivarcs@gmail.com>
2025-03-20 01:00:10 +0530

    Added Coming Soon page

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("scroll", () => {
     const navbar = document.querySelector(".navbar");
-
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 50) {
-            navbar.style.background = "rgba(0, 0, 0, 0.95)";
-        } else {
-            navbar.style.background = "rgba(0, 0, 0, 0.8)";
-        }
-    });
+    if (window.scrollY > 50) {
+        navbar.style.background = "rgba(0, 0, 0, 0.85)";
+    } else {
+        navbar.style.background = "rgb(59, 59, 61)";
+    }
 });
