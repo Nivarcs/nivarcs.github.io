e032dda41df21427a8a8ebe4f41380f8320eade6
Nivarcs <ai.tech.nivarcs@gmail.com>
2025-03-20 16:18:38 +0530

    Updated CSS

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+/* Smooth Scroll Effect */
 document.addEventListener("scroll", () => {
     const navbar = document.querySelector(".navbar");
     if (window.scrollY > 50) {
-        navbar.style.background = "rgba(0, 0, 0, 0.9)";
+        navbar.style.background = "rgba(0, 0, 0, 0.85)";
     } else {
-        navbar.style.background = "rgba(0, 0, 0, 0.8)";
+        navbar.style.background = "rgb(20, 20, 20)";
     }
 });
