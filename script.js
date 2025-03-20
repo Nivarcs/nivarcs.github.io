deb97730758231ca3512b01b8d4a8cd958fbd252
Nivarcs <ai.tech.nivarcs@gmail.com>
2025-03-20 15:29:49 +0530

    Updated CSS

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("scroll", () => {
     const navbar = document.querySelector(".navbar");
     if (window.scrollY > 50) {
-        navbar.style.background = "rgba(0, 0, 0, 0.85)";
+        navbar.style.background = "rgba(0, 0, 0, 0.9)";
     } else {
-        navbar.style.background = "rgb(59, 59, 61)";
+        navbar.style.background = "rgba(0, 0, 0, 0.8)";
     }
 });
